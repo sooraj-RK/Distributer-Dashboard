120f8f85a202c4e653ae59882808e19503adc2d0
saniya262 <saniyasani262@gmail.com>
2025-05-07 18:15:47 +0530

    storeview page created

--- src/components/common/tables/PriceTable.jsx
+++ src/components/common/tables/PriceTable.jsx
@@ -23,43 +23,82 @@ const PriceTable = ({
 
   return (
     <div className={`overflow-x-auto bg-white rounded shadow ${className}`}>
-      <table className="min-w-full text-left">
-        <thead className="bg-[#F6F6F6]">
+      {/* Add table-fixed and specify widths */}
+      <table className="min-w-full text-left max-h-[489px] table-fixed">
+        <thead className="bg-[#F6F6F6] h-6">
           <tr>
-            <th className="p-3  bg-gray-50 text-gray-500 text-sm font-inter ">Product <SouthOutlined fontSize="small" className="text-gray-400 p-[3px]" /></th>
-            <th className="p-3  bg-gray-50 text-gray-500 text-sm font-inter">Units Ordered <SouthOutlined fontSize="small" className="text-gray-400 p-[3px]" /></th>
-            <th className="p-3  bg-gray-50 text-gray-500 text-sm font-inter">Cost per Unit <SouthOutlined fontSize="small" className="text-gray-400 p-[3px]" /></th>
-            <th className="p-3  bg-gray-50 text-gray-500 text-sm font-inter">Total Price <SouthOutlined fontSize="small" className="text-gray-400 p-[3px]" /></th>
+            {/* Product Column - 330.25px */}
+            <th 
+              className="  p-2 text-gray-500 text-sm "
+            >
+              Product <SouthOutlined fontSize="small" className="text-gray-400 p-[1px]" />
+            </th>
+            
+            {/* Units Ordered Column - 100px */}
+            <th 
+              className="  p-2  text-gray-500 text-sm "
+            >
+              Units Ordered <SouthOutlined fontSize="small" className="text-gray-400 p-[1px]" />
+            </th>
+            
+            {/* Cost per Unit Column - 120px */}
+            <th 
+              className="  p-2 text-gray-500 text-sm "
+            >
+              Cost per Unit <SouthOutlined fontSize="small" className="text-gray-400 p-[1px]" />
+            </th>
+            
+            {/* Total Price Column - 120px */}
+            <th 
+              className=" p-2 text-gray-500 text-sm "
+            >
+              Total Price <SouthOutlined fontSize="small" className="text-gray-400 p-[1px]" />
+            </th>
           </tr>
         </thead>
+
         <tbody>
           {rows.map(({ id, product, units, costPerUnit, rowTotal }) => (
-            <tr key={id} className="border-b last:border-none border-gray-200">
-              <td className="p-3 text-gray-500">{product}</td>
-              <td className="p-3 text-gray-500">
+            <tr 
+              key={id} 
+              className="h-6 border-b last:border-none border-gray-200" // Fixed row height
+            >
+              {/* Product Cell */}
+              <td className=" h-6 p-1 text-gray-500 truncate">{product}</td>
+              
+              {/* Units Ordered Cell */}
+              <td className=" h-6 p-1 text-gray-500">
                 <input
-                  type="number"
+                    type="number"
                   min="0"
-                  className="w-16 p-1 border border-gray-200 text-center rounded"
+                  className="w-15  border ps-3 border-gray-200 text-center rounded"
                   value={units}
                   onChange={e => onUnitsChange(id, Number(e.target.value))}
-                />
+                  />
               </td>
-              <td className="p-3 text-gray-500">
+              
+              {/* Cost per Unit Cell */}
+              <td className=" h-6 p-1 text-gray-500">
                 {costPerUnit} {currency}
               </td>
-              <td className="p-3  text-gray-500">
+              
+              {/* Total Price Cell */}
+              <td className=" h-6 p-1 text-gray-500">
                 {rowTotal} {currency}
               </td>
             </tr>
           ))}
         </tbody>
-        <tfoot className="bg-gray-50">
-          <tr>
-            <td className="p-3  bg-gray-50 text-gray-500 text-sm font-inter">Total</td>
-            <td className="p-3  bg-gray-50 text-gray-500 text-sm font-inter">{totalUnits}</td>
-            <td className="p-3  bg-gray-50 text-gray-500 text-sm font-inter" /> {/* empty cell */}
-            <td className="p-3  bg-gray-50 text-gray-500 text-sm font-inter">{totalPrice} {currency}</td>
+
+        {/* Footer with fixed dimensions */}
+        <tfoot className="bg-[#F6F6F6]">
+          <tr className="h-6">
+            <td className="  p-2 text-gray-500 text-sm ">Total</td>
+            <td className="  p-2  text-gray-500 text-sm ">{totalUnits}</td>
+            <td className=" p-2 " /> {/* Empty cell */}
+            <td className="  p-2  text-gray-500 text-sm ">
+              {totalPrice} {currency}
+            </td>
           </tr>
         </tfoot>
       </table>
